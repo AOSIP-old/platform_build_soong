a5a9f9dd1614e54c0c5123da2aa8146682ef95bc
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 14:44:18 -0400

    soong: Include Qualcomm product variables
    Change-Id: Ic514f609257ca5d49174ae996e8adfa9dfb7b540

--- android/variable.go
+++ android/variable.go
@@ -99,6 +99,7 @@ type variableProperties struct {
 
 		// include Aosip variables
 		Aosip android.Product_variables
+		Qualcomm android_aosip.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -178,6 +179,7 @@ type productVariables struct {
 
 	// include Aosip variables
 	Aosip android.ProductVariables
+	Qualcomm android_aosip.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
