b7228dbed7443e8c52d358d3b8d4bdf080ee9fb1
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 22:26:32 -0400

    soong: Rename our android package to android_aosip
    Change-Id: Id5f9577b538820fcee351d6bea25a9626cf2dc49

--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"aosip/soong/android"
+	"aosip/soong/android_aosip"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -98,7 +98,7 @@ type variableProperties struct {
 		}
 
 		// include Aosip variables
-		Aosip android.Product_variables
+		Aosip android_aosip.Product_variables
 		Qualcomm android_aosip.Product_variables
 	} `android:"arch_variant"`
 }
@@ -178,7 +178,7 @@ type productVariables struct {
 	BoardUsesQTIHardware *bool `json:",omitempty"`
 
 	// include Aosip variables
-	Aosip android.ProductVariables
+	Aosip android_aosip.ProductVariables
 	Qualcomm android_aosip.ProductVariables
 }
 
